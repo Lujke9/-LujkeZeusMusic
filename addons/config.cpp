41c0281924948eaf02eb152e67e17a292c5a9acc
Lujke <luke.talbot.c98@gmail.com>
2019-05-16 11:45:27 +0930

    Initial commit

---
+++ addons/config.cpp
@@ -0,0 +1,80 @@
+class CfgPatches
+{
+	class LujkeZeusMusic
+	{
+		name = "Lujkes Custom Music";
+		author = "Lujke";
+		requiredVersion = 1.00;
+		requiredAddons[] = {};
+		units[] = {};
+		weapons[] = {};
+      	        worlds[] = {};
+	};
+};
+
+class CfgMusicClasses
+{
+	class MyMusicClass1
+	{
+		displayName = "Lujke's Custom Music";
+	};
+};
+
+class CfgMusic
+{
+	class weAreOverwatch
+	{
+		name = "We Are Overwatch";
+		sound[] = {"addons\We Are Overwatch.ogg",1,1};
+		duration=50;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class chant
+	{
+		name = "Gregorian Chant";
+		sound[] = {"addons\gregorian-chant-by-kevin-macleod.ogg",1,1};
+		duration=184;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class heroDown
+	{
+		name = "Hero Down";
+		sound[] = {"addons\hero-down-by-kevin-macleod.ogg",1,1};
+		duration=210;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class hitman
+	{
+		name = "Hitman";
+		sound[] = {"addons\hitman-by-kevin-macleod.ogg",1,1};
+		duration=200;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class instinct
+	{
+		name = "Instinct";
+		sound[] = {"addons\instinct-bensound.ogg",1,1};
+		duration=204;
+		musicClass = "MyMusicClass1";
+	};	
+	
+	class showdown
+	{
+		name = "Showdown";
+		sound[] = {"addons\showdown-by-kevin-macleod.ogg",1,1};
+		duration=73;
+		musicClass = "MyMusicClass1";
+	};	
+	
+	class descent
+	{
+		name = "The Descent";
+		sound[] = {"addons\the-descent-by-kevin-macleod.ogg",1,1};
+		duration=191;
+		musicClass = "MyMusicClass1";
+	};
+};
---
+++ addons/mod.cpp
@@ -0,0 +1,12 @@
+name = "Lujkes Custom Music";
+picture = "logo.paa";
+description = "A bunch of music tracks that Lujke thought would be nice in some missions";
+logo = "logo.paa";
+logoOver = "logo.paa";
+tooltip = "Lujkes Custom Music";
+tooltipOwned = "Lujkes Custom Music";
+overview = "Lujkes Custom Music";
+author = "Lujke";
+overviewPicture = "logo.paa";
+overviewText = "Lujkes Custom Music";
+overviewFootnote = "Lujkes Custom Music";
