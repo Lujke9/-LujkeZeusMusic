e69c4ca43aeaff7bfd42d365af4d5a792f16e220
Lujke <luke.talbot.c98@gmail.com>
2019-06-10 04:26:35 +0930

    Added/converted songs
    Configs changed accordingly

--- addons/config.cpp
+++ addons/config.cpp
@@ -22,13 +22,15 @@ class CfgMusicClasses
 
 class CfgMusic
 {
-//	class 
-//	{
-//		name = "";
-//		sound[] = {"addons\.ogg",1,1};
-//		duration=;
-//		musicClass = "MyMusicClass1";
-//	};	
+/*
+	class 
+	{
+		name = "";
+		sound[] = {"addons\.ogg",1,1};
+		duration=;
+		musicClass = "MyMusicClass1";
+	};	
+*/
 	
 	class weAreOverwatch
 	{
@@ -117,4 +119,173 @@ class CfgMusic
 		duration=184;
 		musicClass = "MyMusicClass1";
 	};
+	
+	class arrival
+	{
+		name = "Arrival";
+		sound[] = {"addons\Arrival - Halo 4.ogg",1,1};
+		duration=334;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class bioUnit
+	{
+		name = "Bio Unit";
+		sound[] = {"addons\Bio_Unit - Kevin Macleod.ogg",1,1};
+		duration=209;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class surf
+	{
+		name = "Charlie Don't Surf";
+		sound[] = {"addons\Charlie Don't Surf - COD4.ogg",1,1};
+		duration=100;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class daredevil
+	{
+		name = "Daredevil";
+		sound[] = {"addons\Daredevil - ACE combat.ogg",1,1};
+		duration=307;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class maze
+	{
+		name = "Enter the Maze";
+		sound[] = {"addons\Enter the Maze - Kevin Macleod.ogg",1,1};
+		duration=178;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class ghostsOfReach
+	{
+		name = "Ghosts of Reach";
+		sound[] = {"addons\Ghosts of Reach - Halo legends.ogg",1,1};
+		duration=82;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class amberClad
+	{
+		name = "In Amber Clad";
+		sound[] = {"addons\In Amber Clad - Halo 2.ogg",1,1};
+		duration=99;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class timeComing
+	{
+		name = "Long Time Coming";
+		sound[] = {"addons\Long Time Coming - Kevin Macleod.ogg",1,1};
+		duration=274;
+		musicClass = "MyMusicClass1";
+	};
+		
+	class 
+	{
+		name = "Machines and Might";
+		sound[] = {"addons\Machines and Might - Halo legends.ogg",1,1};
+		duration=62;
+		musicClass = "MyMusicClass1";
+	};
+
+	class finalEffort
+	{
+		name = "One Final Effort - Extended";
+		sound[] = {"addons\One Final Effort - Extended - Halo 3.ogg",1,1};
+		duration=240;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class perchanceDream
+	{
+		name = "Perchance To Dream";
+		sound[] = {"addons\Perchance To Dream - Halo OST.ogg",1,1};
+		duration=116;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class Peril
+	{
+		name = "Peril";
+		sound[] = {"addons\Peril - Halo 2.ogg",1,1};
+		duration=166;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class phantom
+	{
+		name = "Phantom from Space";
+		sound[] = {"addons\Phantom from Space - Kevin Macleod.ogg",1,1};
+		duration=156;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class SCOTLAND
+	{
+		name = "SCOTLAND THE BRAVE";
+		sound[] = {"addons\SCOTLAND THE BRAVE.ogg",1,1};
+		duration=163;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class spirit
+	{
+		name = "Spirit of Fire";
+		sound[] = {"addons\Spirit of Fire - Halo wars.ogg",1,1};
+		duration=131;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class takeLead
+	{
+		name = "Take the Lead";
+		sound[] = {"addons\Take the Lead - Kevin Macleod.ogg",1,1};
+		duration=224;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class aTeam
+	{
+		name = "The A-Team Theme";
+		sound[] = {"addons\The A-Team Full Theme - The Daniel Caine Orchestra.ogg",1,1};
+		duration=192;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class gunAtUniverse
+	{
+		name = "The Gun Pointed At The Head Of The Universe";
+		sound[] = {"addons\The Gun Pointed At The Head Of The Universe - Halo OST.ogg",1,1};
+		duration=146;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class storm
+	{
+		name = "The Storm";
+		sound[] = {"addons\The Storm - Halo 3.ogg",1,1};
+		duration=128;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class unforgotten
+	{
+		name = "Unforgotten";
+		sound[] = {"addons\Unforgotten - Halo 2.ogg",1,1};
+		duration=131;
+		musicClass = "MyMusicClass1";
+	};
+	
+	class zero
+	{
+		name = "Zero";
+		sound[] = {"addons\Zero - ACE combat.ogg",1,1};
+		duration=295;
+		musicClass = "MyMusicClass1";
+	};
+		
 };
