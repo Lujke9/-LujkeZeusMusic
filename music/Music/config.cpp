595ea8918141d91f6da4736cb3ef34353e241f50
Luke <luke.talbot.c98@gmail.com>
2022-10-20 14:47:52 +1030

    Folder restructure

--- music/Music/config.cpp
+++ music/Music/config.cpp
@@ -2,7 +2,7 @@ class CfgPatches
 {
     class LujkeZeusMusic
     {
-        name = "Lujkes Custom Music";
+        name = "Lujke's Custom Music Mod";
         author = "Lujke";
         requiredVersion = 1.00;
         requiredAddons[] = {};
@@ -62,7 +62,7 @@ class CfgMusic
     class xxx
     {
         name = "";
-        sound[] = {"\addons\music\.ogg",1,1};
+        sound[] = {"\music\.ogg",1,1};
         duration=;
         musicClass = "SFPMusic";
     };
@@ -73,7 +73,7 @@ class CfgMusic
     class endless
     {
         name = "Endless";
-        sound[] = {"\addons\music\Halo Infinite\Endless.ogg",1,1};
+        sound[] = {"\music\Halo Infinite\Endless.ogg",1,1};
         duration = 262;
         musicClass = "Infinite";
     };
@@ -81,7 +81,7 @@ class CfgMusic
     class knowMyLegend
     {
         name = "Endless";
-        sound[] = {"\addons\music\Halo Infinite\Know My Legend.ogg",1,1};
+        sound[] = {"\music\Halo Infinite\Know My Legend.ogg",1,1};
         duration = 136;
         musicClass = "Infinite";
     };
@@ -89,7 +89,7 @@ class CfgMusic
     class theRoad
     {
         name = "The Road";
-        sound[] = {"\addons\music\Halo Infinite\The Road.ogg",1,1};
+        sound[] = {"\music\Halo Infinite\The Road.ogg",1,1};
         duration = 126;
         musicClass = "Infinite";
     };
@@ -97,7 +97,7 @@ class CfgMusic
     class weAreOverwatch
     {
         name = "We Are Overwatch";
-        sound[] = {"\addons\music\other\We Are Overwatch.ogg",1,1};
+        sound[] = {"\music\other\We Are Overwatch.ogg",1,1};
         duration = 50;
         musicClass = "SFPMusic";
     };
@@ -105,7 +105,7 @@ class CfgMusic
     class chant
     {
         name = "Gregorian Chant";
-        sound[] = {"\addons\music\other\gregorian-chant-by-kevin-macleod.ogg",1,1};
+        sound[] = {"\music\other\gregorian-chant-by-kevin-macleod.ogg",1,1};
         duration = 184;
         musicClass = "SFPMusic";
     };
@@ -113,7 +113,7 @@ class CfgMusic
     class heroDown
     {
         name = "Hero Down";
-        sound[] = {"\addons\music\other\hero-down-by-kevin-macleod.ogg",1,1};
+        sound[] = {"\music\other\hero-down-by-kevin-macleod.ogg",1,1};
         duration = 210;
         musicClass = "SFPMusic";
     };
@@ -121,7 +121,7 @@ class CfgMusic
     class hitman
     {
         name = "Hitman";
-        sound[] = {"\addons\music\other\hitman-by-kevin-macleod.ogg",1,1};
+        sound[] = {"\music\other\hitman-by-kevin-macleod.ogg",1,1};
         duration = 200;
         musicClass = "SFPMusic";
     };
@@ -129,7 +129,7 @@ class CfgMusic
     class instinct
     {
         name = "Instinct";
-        sound[] = {"\addons\music\other\instinct-bensound.ogg",1,1};
+        sound[] = {"\music\other\instinct-bensound.ogg",1,1};
         duration = 204;
         musicClass = "SFPMusic";
     };
@@ -137,7 +137,7 @@ class CfgMusic
     class showdown
     {
         name = "Showdown";
-        sound[] = {"\addons\music\other\showdown-by-kevin-macleod.ogg",1,1};
+        sound[] = {"\music\other\showdown-by-kevin-macleod.ogg",1,1};
         duration = 73;
         musicClass = "SFPMusic";
     };
@@ -145,7 +145,7 @@ class CfgMusic
     class descent
     {
         name = "The Descent";
-        sound[] = {"\addons\music\other\the-descent-by-kevin-macleod.ogg",1,1};
+        sound[] = {"\music\other\the-descent-by-kevin-macleod.ogg",1,1};
         duration = 191;
         musicClass = "SFPMusic";
     };
@@ -153,7 +153,7 @@ class CfgMusic
     class gate
     {
         name = "Gate Theme Song";
-        sound[] = {"\addons\music\other\gate.ogg",1,1};
+        sound[] = {"\music\other\gate.ogg",1,1};
         duration = 191;
         musicClass = "SFPMusic";
     };
@@ -161,7 +161,7 @@ class CfgMusic
     class advanceAus
     {
         name = "Advance Australia";
-        sound[] = {"\addons\music\other\advance australia.ogg",1,1};
+        sound[] = {"\music\other\advance australia.ogg",1,1};
         duration = 191;
         musicClass = "SFPMusic";
     };
@@ -169,7 +169,7 @@ class CfgMusic
     class aidan
     {
         name = "Light of Aidan";
-        sound[] = {"\addons\music\halo 3 odst\Light of Aidan.ogg",1,1};
+        sound[] = {"\music\halo 3 odst\Light of Aidan.ogg",1,1};
         duration = 184;
         musicClass = "Halo3ODST";
     };
@@ -177,7 +177,7 @@ class CfgMusic
     class arrival
     {
         name = "Arrival";
-        sound[] = {"\addons\music\halo other\Arrival - Halo 4.ogg",1,1};
+        sound[] = {"\music\halo other\Arrival - Halo 4.ogg",1,1};
         duration = 334;
         musicClass = "HaloOther";
     };
@@ -185,7 +185,7 @@ class CfgMusic
     class bioUnit
     {
         name = "Bio Unit";
-        sound[] = {"\addons\music\other\Bio_Unit - Kevin Macleod.ogg",1,1};
+        sound[] = {"\music\other\Bio_Unit - Kevin Macleod.ogg",1,1};
         duration = 209;
         musicClass = "SFPMusic";
     };
@@ -193,7 +193,7 @@ class CfgMusic
     class surf
     {
         name = "Charlie Don't Surf";
-        sound[] = {"\addons\music\other\Charlie Don't Surf - COD4.ogg",1,1};
+        sound[] = {"\music\other\Charlie Don't Surf - COD4.ogg",1,1};
         duration = 100;
         musicClass = "SFPMusic";
     };
@@ -201,7 +201,7 @@ class CfgMusic
     class daredevil
     {
         name = "Daredevil - Ace Combat";
-        sound[] = {"\addons\music\other\Daredevil - ACE combat.ogg",1,1};
+        sound[] = {"\music\other\Daredevil - ACE combat.ogg",1,1};
         duration = 307;
         musicClass = "SFPMusic";
     };
@@ -209,7 +209,7 @@ class CfgMusic
     class maze
     {
         name = "Enter the Maze";
-        sound[] = {"\addons\music\other\Enter the Maze - Kevin Macleod.ogg",1,1};
+        sound[] = {"\music\other\Enter the Maze - Kevin Macleod.ogg",1,1};
         duration = 178;
         musicClass = "SFPMusic";
     };
@@ -217,7 +217,7 @@ class CfgMusic
     class ghostsOfReach
     {
         name = "Ghosts of Reach";
-        sound[] = {"\addons\music\halo other\Ghosts of Reach - Halo legends.ogg",1,1};
+        sound[] = {"\music\halo other\Ghosts of Reach - Halo legends.ogg",1,1};
         duration = 82;
         musicClass = "HaloOther";
     };
@@ -225,7 +225,7 @@ class CfgMusic
     class amberClad
     {
         name = "In Amber Clad";
-        sound[] = {"\addons\music\halo 2\In Amber Clad - Halo 2.ogg",1,1};
+        sound[] = {"\music\halo 2\In Amber Clad - Halo 2.ogg",1,1};
         duration = 99;
         musicClass = "Halo2";
     };
@@ -233,7 +233,7 @@ class CfgMusic
     class timeComing
     {
         name = "Long Time Coming";
-        sound[] = {"\addons\music\other\Long Time Coming - Kevin Macleod.ogg",1,1};
+        sound[] = {"\music\other\Long Time Coming - Kevin Macleod.ogg",1,1};
         duration = 274;
         musicClass = "SFPMusic";
     };
@@ -241,7 +241,7 @@ class CfgMusic
     class machine
     {
         name = "Machines and Might";
-        sound[] = {"\addons\music\hlao other\Machines and Might - Halo legends.ogg",1,1};
+        sound[] = {"\music\hlao other\Machines and Might - Halo legends.ogg",1,1};
         duration = 62;
         musicClass = "HaloOther";
     };
@@ -249,7 +249,7 @@ class CfgMusic
     class finalEffort
     {
         name = "One Final Effort - Extended";
-        sound[] = {"\addons\music\halo 3\One Final Effort - Extended - Halo 3.ogg",1,1};
+        sound[] = {"\music\halo 3\One Final Effort - Extended - Halo 3.ogg",1,1};
         duration = 240;
         musicClass = "Halo3";
     };
@@ -257,7 +257,7 @@ class CfgMusic
     class perchanceDream
     {
         name = "Perchance To Dream";
-        sound[] = {"\addons\music\halo ce\Perchance To Dream - Halo OST.ogg",1,1};
+        sound[] = {"\music\halo ce\Perchance To Dream - Halo OST.ogg",1,1};
         duration = 116;
         musicClass = "HaloCE";
     };
@@ -265,7 +265,7 @@ class CfgMusic
     class peril
     {
         name = "Peril";
-        sound[] = {"\addons\music\halo 2\Peril - Halo 2.ogg",1,1};
+        sound[] = {"\music\halo 2\Peril - Halo 2.ogg",1,1};
         duration = 166;
         musicClass = "Halo2";
     };
@@ -273,7 +273,7 @@ class CfgMusic
     class phantom
     {
         name = "Phantom from Space";
-        sound[] = {"\addons\music\other\Phantom from Space - Kevin Macleod.ogg",1,1};
+        sound[] = {"\music\other\Phantom from Space - Kevin Macleod.ogg",1,1};
         duration = 156;
         musicClass = "SFPMusic";
     };
@@ -281,7 +281,7 @@ class CfgMusic
     class SCOTLAND
     {
         name = "Scotland the Brave";
-        sound[] = {"\addons\music\other\SCOTLAND THE BRAVE.ogg",1,1};
+        sound[] = {"\music\other\SCOTLAND THE BRAVE.ogg",1,1};
         duration = 163;
         musicClass = "SFPMusic";
     };
@@ -289,7 +289,7 @@ class CfgMusic
     class spirit
     {
         name = "Spirit of Fire";
-        sound[] = {"\addons\music\halo other\Spirit of Fire - Halo wars.ogg",1,1};
+        sound[] = {"\music\halo other\Spirit of Fire - Halo wars.ogg",1,1};
         duration = 131;
         musicClass = "HaloOther";
     };
@@ -297,7 +297,7 @@ class CfgMusic
     class takeLead
     {
         name = "Take the Lead";
-        sound[] = {"\addons\music\other\Take the Lead - Kevin Macleod.ogg",1,1};
+        sound[] = {"\music\other\Take the Lead - Kevin Macleod.ogg",1,1};
         duration = 224;
         musicClass = "SFPMusic";
     };
@@ -305,7 +305,7 @@ class CfgMusic
     class aTeam
     {
         name = "The A-Team Theme";
-        sound[] = {"\addons\music\other\The A-Team Full Theme - The Daniel Caine Orchestra.ogg",1,1};
+        sound[] = {"\music\other\The A-Team Full Theme - The Daniel Caine Orchestra.ogg",1,1};
         duration = 192;
         musicClass = "SFPMusic";
     };
@@ -313,7 +313,7 @@ class CfgMusic
     class gunAtUniverse
     {
         name = "The Gun Pointed At The Head Of The Universe";
-        sound[] = {"\addons\music\halo ce\The Gun Pointed At The Head Of The Universe - Halo OST.ogg",1,1};
+        sound[] = {"\music\halo ce\The Gun Pointed At The Head Of The Universe - Halo OST.ogg",1,1};
         duration = 146;
         musicClass = "HaloCE";
     };
@@ -321,7 +321,7 @@ class CfgMusic
     class storm
     {
         name = "The Storm";
-        sound[] = {"\addons\music\halo 3\The Storm - Halo 3.ogg",1,1};
+        sound[] = {"\music\halo 3\The Storm - Halo 3.ogg",1,1};
         duration = 128;
         musicClass = "Halo3";
     };
@@ -329,7 +329,7 @@ class CfgMusic
     class unforgotten
     {
         name = "Unforgotten";
-        sound[] = {"\addons\music\halo 2\Unforgotten - Halo 2.ogg",1,1};
+        sound[] = {"\music\halo 2\Unforgotten - Halo 2.ogg",1,1};
         duration = 131;
         musicClass = "Halo2";
     };
@@ -337,7 +337,7 @@ class CfgMusic
     class zero
     {
         name = "Zero - Ace Combat";
-        sound[] = {"\addons\music\other\Zero - ACE combat.ogg",1,1};
+        sound[] = {"\music\other\Zero - ACE combat.ogg",1,1};
         duration = 295;
         musicClass = "SFPMusic";
     };
@@ -345,7 +345,7 @@ class CfgMusic
     class BF3
     {
         name = "Battlefield 3 Theme";
-        sound[] = {"\addons\music\other\BF3 - Solomon's Theme.ogg",1,1};
+        sound[] = {"\music\other\BF3 - Solomon's Theme.ogg",1,1};
         duration = 172;
         musicClass = "SFPMusic";
     };
@@ -353,7 +353,7 @@ class CfgMusic
     class paleHorse
     {
         name = "Halo - On a Pale Horse";
-        sound[] = {"\addons\music\halo ce\Halo - On a Pale Horse.ogg",1,1};
+        sound[] = {"\music\halo ce\Halo - On a Pale Horse.ogg",1,1};
         duration = 95;
         musicClass = "HaloCE";
     };
@@ -361,7 +361,7 @@ class CfgMusic
     class impend
     {
         name = "Halo 2 - Impend";
-        sound[] = {"\addons\music\halo 2\Halo 2 - Impend.ogg",1,1};
+        sound[] = {"\music\halo 2\Halo 2 - Impend.ogg",1,1};
         duration = 141;
         musicClass = "Halo2";
     };
@@ -369,7 +369,7 @@ class CfgMusic
     class WTMain
     {
         name = "War Thunder - Main Theme";
-        sound[] = {"\addons\music\other\War Thunder - Main Theme.ogg",1,1};
+        sound[] = {"\music\other\War Thunder - Main Theme.ogg",1,1};
         duration = 181;
         musicClass = "SFPMusic";
     };
@@ -377,7 +377,7 @@ class CfgMusic
     class epilogue
     {
         name = "Epilogue";
-        sound[] = {"\addons\music\halo 2\Epilogue.ogg",1,1};
+        sound[] = {"\music\halo 2\Epilogue.ogg",1,1};
         duration = 229;
         musicClass = "Halo2";
     };
@@ -385,7 +385,7 @@ class CfgMusic
     class hereticHero
     {
         name = "Heretic, Hero";
-        sound[] = {"\addons\music\halo 2\Halo 2 Soundtrack - Heretic, Hero.ogg",1,1};
+        sound[] = {"\music\halo 2\Halo 2 Soundtrack - Heretic, Hero.ogg",1,1};
         duration = 154;
         musicClass = "Halo2";
     };
@@ -393,7 +393,7 @@ class CfgMusic
     class lastSpartan
     {
         name = "The Last Spartan";
-        sound[] = {"\addons\music\halo 2\Halo 2 The Last Spartan.ogg",1,1};
+        sound[] = {"\music\halo 2\Halo 2 The Last Spartan.ogg",1,1};
         duration = 138;
         musicClass = "Halo2";
     };
@@ -401,7 +401,7 @@ class CfgMusic
     class endingTribute
     {
         name = "Ending (Tribute)";
-        sound[] = {"\addons\music\halo 3\Halo 3 - Ending (Tribute).ogg",1,1};
+        sound[] = {"\music\halo 3\Halo 3 - Ending (Tribute).ogg",1,1};
         duration = 172;
         musicClass = "Halo3";
     };
@@ -409,7 +409,7 @@ class CfgMusic
     class openSuite
     {
         name = "Opening Suite";
-        sound[] = {"\addons\music\halo CE\Opening Suite.ogg",1,1};
+        sound[] = {"\music\halo CE\Opening Suite.ogg",1,1};
         duration = 58;
         musicClass = "HaloCE";
     };
@@ -417,7 +417,7 @@ class CfgMusic
     class madrigal
     {
         name = "The siege of Madrigal";
-        sound[] = {"\addons\music\halo other\The siege of Madrigal.ogg",1,1};
+        sound[] = {"\music\halo other\The siege of Madrigal.ogg",1,1};
         duration = 64;
         musicClass = "HaloOther";
     };
@@ -425,7 +425,7 @@ class CfgMusic
     class themeMashup
     {
         name = "Ultimate Theme Mashup";
-        sound[] = {"\addons\music\halo other\Ultimate Theme Mashup.ogg",1,1};
+        sound[] = {"\music\halo other\Ultimate Theme Mashup.ogg",1,1};
         duration = 421;
         musicClass = "HaloOther";
     };
@@ -433,7 +433,7 @@ class CfgMusic
     class underNight
     {
         name = "Under Cover Of Night";
-        sound[] = {"\addons\music\halo ce\Under Cover Of Night.ogg",1,1};
+        sound[] = {"\music\halo ce\Under Cover Of Night.ogg",1,1};
         duration = 221;
         musicClass = "HaloCE";
     };
@@ -441,7 +441,7 @@ class CfgMusic
     class walkInWood
     {
         name = "A Walk In The Woods";
-        sound[] = {"\addons\music\halo ce\A Walk In The Woods.ogg",1,1};
+        sound[] = {"\music\halo ce\A Walk In The Woods.ogg",1,1};
         duration = 113;
         musicClass = "HaloCE";
     };
@@ -449,7 +449,7 @@ class CfgMusic
     class ashes
     {
         name = "Ashes";
-        sound[] = {"\addons\music\reach\Ashes.ogg",1,1};
+        sound[] = {"\music\reach\Ashes.ogg",1,1};
         duration = 166;
         musicClass = "HaloReach";
     };
@@ -457,7 +457,7 @@ class CfgMusic
     class anyCost
     {
         name = "At Any Cost";
-        sound[] = {"\addons\music\reach\At Any Cost.ogg",1,1};
+        sound[] = {"\music\reach\At Any Cost.ogg",1,1};
         duration = 150;
         musicClass = "HaloReach";
     };
@@ -465,7 +465,7 @@ class CfgMusic
     class rain
     {
         name = "Rain (Deference for Darkness)";
-        sound[] = {"\addons\music\halo 3 odst\Rain - Deference for Darkness.ogg",1,1};
+        sound[] = {"\music\halo 3 odst\Rain - Deference for Darkness.ogg",1,1};
         duration = 190;
         musicClass = "Halo3ODST";
     };
@@ -473,7 +473,7 @@ class CfgMusic
     class hardPlace
     {
         name = "Rock In A Hard Place";
-        sound[] = {"\addons\music\halo ce\Rock In A Hard Place.ogg",1,1};
+        sound[] = {"\music\halo ce\Rock In A Hard Place.ogg",1,1};
         duration = 80;
         musicClass = "HaloCE";
     };
@@ -481,7 +481,7 @@ class CfgMusic
     class skyline
     {
         name = "Skyline";
-        sound[] = {"\addons\music\halo 3 odst\Skyline.ogg",1,1};
+        sound[] = {"\music\halo 3 odst\Skyline.ogg",1,1};
         duration = 94;
         musicClass = "Halo3ODST";
     };
@@ -489,7 +489,7 @@ class CfgMusic
     class strung
     {
         name = "Strung";
-        sound[] = {"\addons\music\halo ce\Strung.ogg",1,1};
+        sound[] = {"\music\halo ce\Strung.ogg",1,1};
         duration = 109;
         musicClass = "HaloCE";
     };
@@ -497,7 +497,7 @@ class CfgMusic
     class walkingAway
     {
         name = "Walking Away";
-        sound[] = {"\addons\music\reach\Walking Away.ogg",1,1};
+        sound[] = {"\music\reach\Walking Away.ogg",1,1};
         duration = 113;
         musicClass = "HaloReach";
     };
@@ -505,7 +505,7 @@ class CfgMusic
     class goingAnywhere
     {
         name = "We're Not Going Anywhere";
-        sound[] = {"\addons\music\reach\We're Not Going Anywhere.ogg",1,1};
+        sound[] = {"\music\reach\We're Not Going Anywhere.ogg",1,1};
         duration = 74;
         musicClass = "HaloReach";
     };
